34e92e3c5dbf7319a47e5a80fec0a39fdb4a1bd6
wolf <wolfofbadenoch@gmail.com>
2015-11-28 00:21:44 +0000

    Add initial badger behaviour

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/**
+ * Badger Behaviours
+ *
+ * @param  {String} name
+ * @return {String}
+ */
+
+module.exports = {
+  burrow: function(name) {
+    return "Badger called "+name + " digging deep";
+  }
+
+};
